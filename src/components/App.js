0ac9727f229d321017ac1c4c0d52b8d843c25b5c
Thomas McNish <mcnishthomas@gmail.com>
2022-04-30 09:16:28 -0400

    added home route to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {
+  BroswerRouter,
+  Route,
+  BrowserRouter
+} from 'react-router-dom';
+
+// App components
+import Home from './Home';
 
 const App = () => (
-  <div className="container">
-
-  </div>
+  <BrowserRouter>
+    <div className="container">
+      <Route path="/" component={Home} />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
