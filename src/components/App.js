bbd3d4847bea8256c890eb778fcc9ef3b69a7642
Thomas McNish <mcnishthomas@gmail.com>
2022-04-30 09:20:41 -0400

    Added about route and set exact prop for home route

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,13 @@ import {
 
 // App components
 import Home from './Home';
+import About from './About';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
     </div>
   </BrowserRouter>
 );
