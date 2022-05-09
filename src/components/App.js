22442fdb57873d16565a3bf410d591db5c0284ef
Thomas McNish <mcnishthomas@gmail.com>
2022-05-09 09:11:52 -0400

    added 404 page. Refactored course component to be dynamic

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import {
   Route,
-  BrowserRouter
+  BrowserRouter,
+  Switch
 } from 'react-router-dom';
 
 // App components
 import Header from './Header';
 import Home from './Home';
 import About from './About';
-import Teachers from './Teachers.js';
+import Teachers from './Teachers';
 import Courses from './Courses';
+import NotFound from './NotFound';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header />
-      <Route exact path="/" component={Home} />
-      <Route path="/about" render={() => <About title='About' />} />
-      <Route path="/teachers" component={Teachers} />
-      <Route path="/courses" component={Courses} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" render={() => <About title='About' />} />
+        <Route path="/teachers" component={Teachers} />
+        <Route path="/courses" component={Courses} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
